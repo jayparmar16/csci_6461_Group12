c04e2bb9e7b565289e93e504bf765da39dad9d26
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-09-15 19:48:05 -0400

    Added Main.java entry point for assembler

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String[] args){
+        
+        String filePath = "source.txt";
+        System.out.println("Assembling file: "+ filePath);
+
+        Assembler assembler = new Assembler();
+        assembler.assemble(filePath);
+    }
 }
