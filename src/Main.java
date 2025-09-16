1ac224042264a67a4d993320140a430459904d02
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-09-15 22:07:06 -0400

    formatted the code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         String filePath = "source.txt";
-        System.out.println("Assembling file: "+ filePath);
+        System.out.println("Assembling file: " + filePath);
 
         Assembler assembler = new Assembler();
         assembler.assemble(filePath);
