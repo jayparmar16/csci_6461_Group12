e631886940570685754e2e93ae8ff23db5294250
Jay Parmar <jay_parmar16@hotmail.com>
2025-09-19 12:07:44 -0400

    added code to allow user to select the source file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        String filePath = "source.txt";
-        System.out.println("Assembling file: " + filePath);
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select Assembler Source File");
+        fileChooser.setCurrentDirectory(new File(".")); // Start in current directory
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Text Files", "txt"));
 
-        Assembler assembler = new Assembler();
-        assembler.assemble(filePath);
+        int result = fileChooser.showOpenDialog(frame);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+
+            Assembler assembler = new Assembler();
+            assembler.assemble(selectedFile.getAbsolutePath());
+        } else {
+            System.out.println("No file selected. Exiting.");
+        }
+
+        frame.dispose(); // Close the hidden frame
     }
 }
