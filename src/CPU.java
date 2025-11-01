cfe655eaaf38a0f97e3e679e7be3393d9099f17f
Siddhesh Parab <parabsiddhesh95@gmail.com>
2025-11-01 19:51:39 -0400

    File Refactored

--- src/CPU.java
+++ src/CPU.java
@@ -680,20 +680,3 @@ public class CPU {
     public void setMBR(short value) { mbr = value; }
     public void setIR(short value) { ir = value; }
 }
-
-/**
- * Utility class for number conversions.
- */
-class Utils {
-    public short octalToShort(String octal) {
-        return (short) Integer.parseInt(octal, 8);
-    }
-
-    public String shortToOctal(short value, int digits) {
-        return String.format("%0" + digits + "o", value);
-    }
-
-    public String shortToBinary(short value, int bits) {
-        return String.format("%" + bits + "s", Integer.toBinaryString(value & 0xFFFF)).replace(' ', '0');
-    }
-}
